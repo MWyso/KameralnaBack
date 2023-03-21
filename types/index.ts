100b278ae525a9ef2a601d2d8ed6dc52c1fa3071
MWyso <mateusz.wyso94@gmail.com>
2023-03-21 16:05:40 +0100

    Create login component and show on the web

--- types/index.ts
+++ types/index.ts
@@ -2,3 +2,4 @@ export * from './info/info.entity';
 export * from './menu/menu.entity';
 export * from './addons/addons.entity';
 export * from './map/map-entity';
+export * from './user/user.entity';
--- utils/errors.ts
+++ utils/errors.ts
@@ -1,14 +1,31 @@
-import {NextFunction, Request, Response} from "express";
+import { NextFunction, Request, Response } from 'express';
+
+interface Error {
+    errno?: number;
+}
 
 export class ValidationError extends Error {}
 
-export const handleError = (err: Error, req: Request, res: Response, next: NextFunction): void => {
-
-    console.error(err);
-
-    res
-        .status(err instanceof ValidationError ? 400 : 500)
-        .json( {
-            message: err instanceof ValidationError ? err.message : 'Sorry, please try again later',
+export const handleError = (err: Error, req: Request, res: Response, next: NextFunction) => {
+    // SQL DUPLICATE EMAIL ERROR
+    if (err.errno === 1062) {
+        res.status(400).json({
+            success: false,
+            message: 'This e-mail exists in the database. Please enter a new one',
         });
+        return;
+    }
+    // VALIDATION ERROR
+    if (err instanceof ValidationError) {
+        res.status(400).json({
+            success: false,
+            message: err.message,
+        });
+        return;
+    }
+    // SERVER ERRORS
+    res.status(500).json({
+        success: false,
+        message: 'Sorry, please try again later.',
+    });
 };
